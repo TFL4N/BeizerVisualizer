8a2d01fe246b7f534902c270913716c355f2b6fb
TFL4N <tflan10@gmail.com>
2018-10-20 18:01:10 -0400

    Initial Commit

---
+++ BezierCurves/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BezierCurves
+//
+//  Created by Spizzace on 10/20/18.
+//  Copyright © 2018 SpaiceMaine. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BezierCurves/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BezierCurves
+//
+//  Created by Spizzace on 10/20/18.
+//  Copyright © 2018 SpaiceMaine. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
